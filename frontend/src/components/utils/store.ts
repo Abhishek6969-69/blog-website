161b5524f5146cd75d9501b1c3c1d828a9b98066
Abhishek6969-69 <sbh123yadav@gmail.com>
2024-10-21 00:03:46 +0530

    hi

--- frontend/src/components/utils/store.ts
+++ frontend/src/components/utils/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersliceReducer from './slice1'; 
 
-export const store = configureStore({
+ const store = configureStore({
   reducer: {
     user: usersliceReducer, // Correct the key here
   },
@@ -9,3 +9,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export default store; 

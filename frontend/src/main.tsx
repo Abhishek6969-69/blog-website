89ddc2118e5abe4cbd8beabf21849946bc634f10
Abhishek6969-69 <sbh123yadav@gmail.com>
2024-10-21 18:46:42 +0530

    finalllll

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,8 @@ import store from './utils/store.ts'
 
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <Provider store={store}>
